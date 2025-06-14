6f03f9769332d58ca3ef1d20b288d935fe0f228e
Prince_ij <aminuibrahimthefifth@gmail.com>
2025-06-14 20:42:36 +0100

    final touches

--- abudevs_api/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# app/models/__init__.py
-from .student import Student
-from .event import Event
-from .blog import Blog
-from .user import User
--- abudevs_api/app/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .student import StudentSchema
-from .event import EventSchema
-from .blog import BlogSchema
-from .user import UserSchema
-
